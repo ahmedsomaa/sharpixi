b2c4a7e7e317ca497f1dc124a3ba7d5f2a60b656
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-11 19:34:00 +0200

    feat: Add eslint configuration for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,19 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'],
-  rules: {}
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 1 // display styling issues as WARN
+  }
 };
