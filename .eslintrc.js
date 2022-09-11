d0bfc306eb58388c4951da3b53216cadcfc16b32
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-11 20:16:10 +0200

    refactor: Extend eslint to use airbnb style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'airbnb',
     'prettier'
   ],
   overrides: [],
@@ -16,6 +17,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': 1 // display styling issues as WARN
+    'prettier/prettier': 1
   }
 };
