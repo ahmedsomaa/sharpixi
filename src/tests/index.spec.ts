b4427b168708fa7fc9ff1b4d9d76b806e06e69c6
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-11 22:31:22 +0200

    feat: Add testing for main route

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,18 @@
+import app from '../index';
+import supertest from 'supertest';
+
+describe('Main Router Tests Suite', () => {
+  const request = supertest(app);
+
+  describe('GET / Request', () => {
+    it('should return 200', async () => {
+      const response = await request.get('/');
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('should return text/html', async () => {
+      const response = await request.get('/');
+      expect(response.type).toBe('text/html');
+    });
+  });
+});
