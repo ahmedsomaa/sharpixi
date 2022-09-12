673e5b6f0b64a742c22514edfc530e7ca8acdfcb
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-13 01:10:57 +0200

    fix: Remove param name from message

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,19 +4,19 @@ export const queryValidator = [
   query('filename')
     .not()
     .isEmpty()
-    .withMessage('filename cannot be empty')
+    .withMessage('cannot be empty')
     .isString()
-    .withMessage('filename msut be a string'),
+    .withMessage('msut be a string'),
   query('height')
     .not()
     .isEmpty()
-    .withMessage('height cannot be empty')
+    .withMessage('cannot be empty')
     .isInt()
-    .withMessage('height must be a number'),
+    .withMessage('must be a number'),
   query('width')
     .not()
     .isEmpty()
-    .withMessage('width cannot be empty')
+    .withMessage('cannot be empty')
     .isInt()
-    .withMessage('width must be a number')
+    .withMessage('must be a number')
 ];
