c3b8233e356be6a38e376001b0669af02eab3767
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-12 05:38:48 +0200

    feat: Config app to use ejs

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import favicon from 'serve-favicon';
+import fs from 'fs';
 import helmet from 'helmet';
 import indexRouter from './routes';
 import morgan from 'morgan';
 import path from 'path';
+import { resolveImageDirectoryPath } from './helpers/image';
 
 // configure dotenv
 dotenv.config();
@@ -15,6 +17,9 @@ dotenv.config();
 // Create express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 // logger
 app.use(morgan('common'));
@@ -35,7 +40,11 @@ app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(indexRouter);
 
 // start app
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
+  const dir = resolveImageDirectoryPath('thumbs');
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
   console.log(`Server started at http://localhost:${PORT} ...`);
 });
 
