3285d9e753838b2322679df05a7172b458f0237c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-12 14:31:34 +0200

    fix: Update main router response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,13 @@
 import express, { Request, Response } from 'express';
 
 import imageRoutes from './api/image';
-import path from 'path';
 
 const indexRoute = express.Router();
 
-// main router
-indexRoute.get('/', (req: Request, res: Response) => {
-  res.render(path.join(__dirname, '..', '..', 'public', 'index'));
-});
-
 // image router
 indexRoute.use('/images', imageRoutes);
+
+// main router
+indexRoute.get('/', (req: Request, res: Response) => res.render('index'));
 
 export default indexRoute;
