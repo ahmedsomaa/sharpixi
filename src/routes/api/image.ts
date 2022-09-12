387dfb7f529c9c263a54b14692bb26394a07ac4a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-12 05:40:40 +0200

    feat: add /all endpoint

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,32 +1,10 @@
-import * as _ from 'lodash';
-
-import express, { Request, Response } from 'express';
-
-import { isEmptyQuery } from '../../util';
+import express from 'express';
+import imageController from '../../controllers/image';
 import { queryValidator } from '../../middleware';
-import { validationResult } from 'express-validator';
 
-const imageRouter = express.Router();
+const imageRoutes = express.Router();
 
-imageRouter.get('/', queryValidator, (req: Request, res: Response) => {
-  // get query params validaion errors
-  const result = validationResult(req).formatWith(({ param, msg }) => ({
-    param,
-    msg
-  }));
-  // check for empty query string
-  if (isEmptyQuery(req.query)) {
-    return res.send('Query string cannot be empty');
+imageRoutes.get('/all', imageController.all);
+imageRoutes.get('/', queryValidator, imageController.resize);
 
-    // check for validation error
-  } else if (!result.isEmpty()) {
-    const errors = result.array();
-    const grouped = _.groupBy(errors, ({ param }) => param);
-    return res.json(grouped);
-  }
-
-  // query string is not empty & no validation errors
-  return res.send('Ok');
-});
-
-export default imageRouter;
+export default imageRoutes;
