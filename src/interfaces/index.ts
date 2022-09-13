e9a0e26290d09b843bd1fe9e52e231c99d57884a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-13 18:04:11 +0200

    feat: Add ConvertQuery, ImageFile, SharpResult & ImageFormat types

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,9 +3,28 @@
 // thumbs -> edited photos
 export type ImageDirectory = 'original' | 'thumbs';
 
-// interface to image query string
-export interface ImageQueryString {
+// supported image formats for conversion
+export type ImageFormat = 'png' | 'jpg' | 'avif' | 'jpeg' | 'webp';
+
+// interface for query string for image resizing
+export interface ResizeQuery {
   width: number;
   height: number;
   filename: string;
+}
+
+// interface for query string for image converison
+export interface ConvertQuery {
+  format: ImageFormat;
+  filename: string;
+}
+
+export interface ImageFile {
+  source: string;
+  target: string;
+}
+export interface SharpResult {
+  data: string;
+  errors?: object;
+  success: boolean;
 }
