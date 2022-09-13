0f66cc9e8fc4d81087558616e8ad2812b2996518
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-13 18:02:27 +0200

    feat: Finish covert functionality

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,16 +1,18 @@
+import { ConvertQuery, ResizeQuery, SharpResult } from '../interfaces';
 import { Request, Response } from 'express';
-import {
-  getAllImages,
-  imageExists,
-  resizeImage,
-  resolveImageDirectoryPath
-} from '../helpers/image.helper';
+import { imageExists, resolveImageDirectoryPath } from '../helpers/image.helper';
 
-import { ImageQueryString } from '../interfaces';
+import SharpService from '../services/sharp.service';
 import _ from 'lodash';
 import path from 'path';
 import { validationResult } from 'express-validator';
 
+/**
+ * Image Controller for Resize Route
+ * @param {Request} req -- http request object to handle
+ * @param {Response} res -- http response to send
+ * @returns {void}
+ */
 const resize = async (req: Request, res: Response) => {
   // get query params validaion errors
   const result = validationResult(req).formatWith(({ param, msg }) => ({
@@ -35,29 +37,52 @@ const resize = async (req: Request, res: Response) => {
   }
 
   // desirialize query string
-  const { filename, width, height } = req.query as unknown as ImageQueryString;
+  const { filename, width, height } = req.query as unknown as ResizeQuery;
 
-  // check for file in public/images/thumbs
-  const inThumbs = await imageExists(`${filename}_${height}x${width}`, 'thumbs');
+  // check for file in images/thumbs
+  const checkThumbs: SharpResult = await imageExists(`${filename}_${height}x${width}`, 'thumbs');
 
-  if (!inThumbs) {
-    const inOriginal = await imageExists(filename, 'original');
-    return inOriginal
-      ? res.sendFile(
-          await resizeImage({
-            filename: inOriginal,
-            width: +width,
-            height: +height
-          })
-        )
-      : res
-          .status(404)
-          .render('error', { code: 404, message: 'File does not exist'.toUpperCase() });
+  if (checkThumbs.success) {
+    // return from thumbs
+    return res.sendFile(path.join(resolveImageDirectoryPath('thumbs'), checkThumbs.data));
   } else {
-    return res.sendFile(path.join(resolveImageDirectoryPath('thumbs'), inThumbs));
+    // found in original, resize it
+    const checkOriginal: SharpResult = await imageExists(filename, 'original');
+
+    if (checkOriginal.success) {
+      // call sharp service to resize image
+      const resizePromise: SharpResult = await SharpService.resize({
+        filename: checkOriginal.data,
+        width,
+        height
+      });
+      if (resizePromise.success) {
+        // image resize -> success -> send resized image
+        return res.sendFile(resizePromise.data);
+      } else {
+        // failed to resize image
+        // let the user know
+        return res.status(400).render('error', {
+          code: 400,
+          errors: resizePromise.errors,
+          message: resizePromise.data.toUpperCase()
+        });
+      }
+    } else {
+      // image doesn't exist
+      return res
+        .status(404)
+        .render('error', { code: 404, message: 'File does not exist'.toUpperCase() });
+    }
   }
 };
 
+/**
+ * Image Controller for Convert Route
+ * @param {Request} req -- http request object to handle
+ * @param {Response} res -- http response to send
+ * @returns {void}
+ */
 const convert = async (req: Request, res: Response) => {
   // get query params validaion errors
   const result = validationResult(req).formatWith(({ param, msg }) => ({
@@ -80,7 +105,38 @@ const convert = async (req: Request, res: Response) => {
       errors: grouped
     });
   }
-  res.send('Converted');
+
+  // desirialize query string
+  const { filename, format } = req.query as unknown as ConvertQuery;
+  // check for file in images/thumbs
+  const checkThumbs: SharpResult = await imageExists(`${filename}_converted.${format}}`, 'thumbs');
+  if (checkThumbs.success) {
+    // return from thumbs
+    return res.sendFile(path.join(resolveImageDirectoryPath('thumbs'), checkThumbs.data));
+  } else {
+    // found in original, resize it
+    const checkOriginal: SharpResult = await imageExists(filename, 'original');
+
+    if (checkOriginal.success) {
+      const convertPromise: SharpResult = await SharpService.convert({
+        filename: checkOriginal.data,
+        format
+      });
+      if (convertPromise.success) {
+        return res.sendFile(convertPromise.data);
+      } else {
+        return res.status(400).render('error', {
+          code: 400,
+          errors: convertPromise.errors,
+          message: convertPromise.data.toUpperCase()
+        });
+      }
+    } else {
+      return res
+        .status(404)
+        .render('error', { code: 404, message: 'File does not exist'.toUpperCase() });
+    }
+  }
 };
 
 export default {
