6874b35873d086c5a390ca9bddd53d9a06a76dc2
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-14 10:25:36 +0200

    feat: Pass converted images to converter page

--- src/controllers/views.controller.ts
+++ src/controllers/views.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { getAllImages } from '../helpers/image.helper';
 import { mapImageWithPath } from '../util';
+import path from 'path';
 
 const resizer = async (req: Request, res: Response) => {
   const original = await getAllImages('original');
@@ -20,9 +21,17 @@ const converter = async (req: Request, res: Response) => {
   const original = await getAllImages('original');
   const thumbs = await getAllImages('thumbs');
   const mapped = original?.map(mapImageWithPath);
+  const converted = thumbs
+    ?.filter((img) => {
+      const file = path.parse(img);
+      const arr = file.name.split('_');
+      return arr[1] === 'converted';
+    })
+    .map(mapImageWithPath);
+
   res.render('converter', {
     original: mapped,
-    thumbs,
+    thumbs: converted,
     year: new Date().getFullYear()
   });
 };
