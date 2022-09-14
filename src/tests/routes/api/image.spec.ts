7c18a0c3cb95ddc78c0adfcfb3570832a85d2b7b
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-14 17:59:32 +0200

    fix: Use destruction for request response

--- src/tests/routes/api/image.spec.ts
+++ src/tests/routes/api/image.spec.ts
@@ -1,64 +1,72 @@
 import app from '../../../index';
-import supertest from 'supertest';
+import request from 'supertest';
 
 describe('-- Image Router Tests Suite', (): void => {
-  const request = supertest(app);
-
   describe('--- GET /api/resize Request', (): void => {
     it('should return 400 for empty query string', async (): Promise<void> => {
-      const response = await request.get('/api/resize');
-      expect(response.status).toEqual(400);
-      expect(response.type).toEqual('text/html');
+      const { type } = await request(app).get('/api/resize');
+      expect(400);
+      expect(type).toEqual('text/html');
     });
 
-    it('should return 422 for invalid query string parameters', async (): Promise<void> => {
-      const response = await request.get('/api/resize?filename=&height=four&width=_4');
-      expect(response.status).toEqual(422);
-      expect(response.type).toEqual('text/html');
+    it('should return 422 for invalid query string', async (): Promise<void> => {
+      const { type } = await request(app).get('/api/resize?filename=&height=four&width=_4');
+      expect(422);
+      expect(type).toEqual('text/html');
     });
 
     it('should return 404 if image does not exist', async (): Promise<void> => {
-      const response = await request.get('/api/resize?filename=myimage&height=200&width=200');
-      expect(response.status).toEqual(404);
-      expect(response.type).toEqual('text/html');
+      const { type } = await request(app).get('/api/resize?filename=myimage&height=200&width=200');
+      expect(404);
+      expect(type).toEqual('text/html');
     });
 
-    it('should return 200 with resized image if image exists', async (): Promise<void> => {
-      const images = ['encenadaport', 'fjord', 'icelandwaterfall', 'palmtunnel', 'santamonica'];
-      images.forEach(async (img) => {
-        const resp = await request.get(`/api/resize?filename=${img}&width=600&height=400`);
-        expect(resp.status).toEqual(200);
-        expect(resp.type).toEqual('image/jpeg');
-      });
+    it('should return 200 with resized image for valid query string', async (): Promise<void> => {
+      const { type } = await request(app).get(
+        `/api/resize?filename=encenadaport&width=600&height=400`
+      );
+      expect(200);
+      expect(type).toEqual('image/jpeg');
+    });
+
+    it('should return 200 with resized image for the same query string', async (): Promise<void> => {
+      const { type } = await request(app).get(
+        `/api/resize?filename=encenadaport&width=600&height=400`
+      );
+      expect(200);
+      expect(type).toEqual('image/jpeg');
     });
   });
 
   describe('--- GET /api/convert Request', (): void => {
     it('should return 400 for empty query string', async (): Promise<void> => {
-      const response = await request.get('/api/convert');
-      expect(response.status).toEqual(400);
-      expect(response.type).toEqual('text/html');
+      const { type } = await request(app).get('/api/convert');
+      expect(400);
+      expect(type).toEqual('text/html');
     });
 
-    it('should return 422 for invalid query string parameters', async (): Promise<void> => {
-      const response = await request.get('/api/convert?filename=&format=hello');
-      expect(response.status).toEqual(422);
-      expect(response.type).toEqual('text/html');
+    it('should return 422 for invalid query string', async (): Promise<void> => {
+      const { type } = await request(app).get('/api/convert?filename=&format=hello');
+      expect(422);
+      expect(type).toEqual('text/html');
     });
 
     it('should return 404 if image does not exist', async (): Promise<void> => {
-      const response = await request.get('/api/convert?filename=myimage&format=png');
-      expect(response.status).toEqual(404);
-      expect(response.type).toEqual('text/html');
+      const { type } = await request(app).get('/api/convert?filename=myimage&format=png');
+      expect(404);
+      expect(type).toEqual('text/html');
     });
 
-    it('should return 200 with converted image if image exists', async (): Promise<void> => {
-      const images = ['encenadaport', 'fjord', 'icelandwaterfall', 'palmtunnel', 'santamonica'];
-      images.forEach(async (img) => {
-        const resp = await request.get(`/api/convert?filename=${img}&format=png`);
-        expect(resp.status).toEqual(200);
-        expect(resp.type).toEqual('image/png');
-      });
+    it('should return 200 with converted image for valid query string', async (): Promise<void> => {
+      const { type } = await request(app).get(`/api/convert?filename=fjord&format=png`);
+      expect(200);
+      expect(type).toEqual('image/png');
+    });
+
+    it('should return 200 with resized image for the same query string', async (): Promise<void> => {
+      const { type } = await request(app).get(`/api/convert?filename=fjord&format=png`);
+      expect(200);
+      expect(type).toEqual('image/png');
     });
   });
 });
