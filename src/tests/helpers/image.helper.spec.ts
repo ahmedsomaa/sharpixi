b192e08f0339d22774f5edff2facc0310fed046d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-14 18:22:34 +0200

    refactor: Update test names

--- src/tests/helpers/image.helper.spec.ts
+++ src/tests/helpers/image.helper.spec.ts
@@ -9,12 +9,12 @@ import {
 describe('-- Image helper Tests Suite', (): void => {
   describe('--- imageExists method', (): void => {
     const filename = 'fjord.jpg';
-    it('sholud return string if file exists in directory', async (): Promise<void> => {
+    it('sholud return file name with extension if file exists in directory', async (): Promise<void> => {
       const resolved: SharpResult = await imageExists('fjord', 'original');
       expect(resolved.success).toEqual(true);
       expect(resolved.data).toEqual(filename);
     });
-    it('should return empty string if file does not exist in directory', async (): Promise<void> => {
+    it('should return "File does not exist" if file does not exist in directory', async (): Promise<void> => {
       const resolved: SharpResult = await imageExists('fjord', 'thumbs');
       expect(resolved.success).toEqual(false);
       expect(resolved.data).toEqual('File does not exist');
@@ -30,7 +30,7 @@ describe('-- Image helper Tests Suite', (): void => {
   });
 
   describe('--- resolveImageDirectoryPath method', (): void => {
-    it('sholud return directory path as string', (): void => {
+    it('sholud return directory full path as string', (): void => {
       const directoryPath = resolveImageDirectoryPath('original');
       expect(directoryPath).toEqual(
         'C:\\Users\\ahmedsomaa\\Documents\\Projects\\eg-fwd\\image-processing-api\\images\\original'
