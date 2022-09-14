1c51ca9126ded2d497fa08530bb36520c77384db
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-14 11:41:32 +0200

    fix: Filter resized images only

--- src/controllers/views.controller.ts
+++ src/controllers/views.controller.ts
@@ -8,7 +8,13 @@ const resizer = async (req: Request, res: Response) => {
   const original = await getAllImages('original');
   const thumbs = await getAllImages('thumbs');
   const mapped = original?.map(mapImageWithPath);
-  const resized = thumbs?.map(mapImageWithPath);
+  const resized = thumbs
+    ?.filter((img) => {
+      const file = path.parse(img);
+      const arr = file.name.split('_');
+      return !(arr[1] === 'converted');
+    })
+    .map(mapImageWithPath);
 
   res.render('index', {
     original: mapped,
