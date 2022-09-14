920cb4a4e4b3548fc266903564c5b97b6157f7be
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-09-14 18:00:12 +0200

    refactor: Use destructre

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,24 +1,24 @@
 import app from '../index';
-import supertest from 'supertest';
+import request from 'supertest';
 
 describe('-- Express App Tests Suits', (): void => {
-  const request = supertest(app);
   it('should serve a favorite icon', async (): Promise<void> => {
-    const response = await request.get('/favicon.ico');
-    expect(response.status).toBe(200);
-    expect(response.type).toBe('image/x-icon');
+    const { type } = await request(app).get('/favicon.ico');
+    expect(200);
+    expect(type).toBe('image/x-icon');
   });
 
   it('should use handlebars as the default view engine', (): void => {
-    expect(app.settings['view engine']).toEqual('hbs');
+    const { settings } = app;
+    expect(settings['view engine']).toEqual('hbs');
   });
 
   it('should serve static files', (): void => {
     const images = ['encenadaport', 'fjord', 'icelandwaterfall', 'palmtunnel', 'santamonica'];
     images.forEach(async (img) => {
-      const response = await request.get(`/original/${img}.jpg`);
-      expect(response.status).toEqual(200);
-      expect(response.type).toEqual('image/jpeg');
+      const { type } = await request(app).get(`/original/${img}.jpg`);
+      expect(200);
+      expect(type).toEqual('image/jpeg');
     });
   });
 });
